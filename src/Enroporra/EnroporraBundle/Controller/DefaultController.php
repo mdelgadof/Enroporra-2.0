fd249d282fc5800aa3295102f8cc83a127be9838
Miguel Delgado Fernandez <miguel.delgado@gmail.com>
2012-09-30 01:20:10 +0200

    Cambios en la plantilla inicial para mostrar los titulares y cuerpos de las noticias activas en la home

--- src/Enroporra/EnroporraBundle/Controller/DefaultController.php
+++ src/Enroporra/EnroporraBundle/Controller/DefaultController.php
@@ -6,14 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+
+    public function baseAction()
     {
         $banner = rand(1,12);
         $year = date("Y");
         $competition = "Eurocopa 2012";
         $contact = "miguel.delgado@gmail.com";
+
         return $this->render('EnroporraBundle:Front:index.html.twig',
             array('banner' => $banner, 'year' => $year, 'competition' => $competition, 'contact' => $contact)
+        );
+    }
+
+    public function indexAction()
+    {
+        $noticias = array();
+
+        return $this->render('EnroporraBundle:Front:index.html.twig',
+            array('noticias' => $noticias)
         );
     }
 }
